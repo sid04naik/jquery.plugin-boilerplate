c9a711ced86862dc686dd5f43589f97e45941db0
Siddhant <naiksiddhant335@gmail.com>
2019-06-20 23:37:17 +0530

    indented the code

--- jquery.plugin-boilerplate.commented.js
+++ jquery.plugin-boilerplate.commented.js
@@ -25,7 +25,7 @@
         plugin wrapper to construct the key for the "$.data" method.
         More: http://api.jquery.com/jquery.data/
     */
-    var pluginName = 'PluginName';
+	var pluginName = 'PluginName';
 
     /*
         The "Plugin" constructor, builds a new instance of the plugin for the
@@ -33,16 +33,16 @@
         "$('selector').pluginName();" creates a new instance of pluginName for the given selector.
     */
 
-    // Create the plugin constructor
-    function Plugin(element, options) {
+	// Create the plugin constructor
+	function Plugin(element, options) {
         /*
             Provide local access to the DOM node(s) that called the plugin,
             as well local access to the pluginName and default options.
         */
 
-        this._element    = element;
-        this._pluginName = pluginName;
-        this._defaults   = $.fn.myPluginName.defaults;
+		this._element    = element;
+		this._pluginName = pluginName;
+		this._defaults   = $.fn.myPluginName.defaults;
         /*
             The "$.extend" method merges the contents of two or more objects,
             and stores the result in the first object. The first object is
@@ -50,7 +50,7 @@
             instances of the plugin.
             More: http://api.jquery.com/jquery.extend/
         */
-        this._settings = $.extend({}, this._defaults, options);
+		this._settings = $.extend({}, this._defaults, options);
 
         /*
             The "_initialize" method is the starting point for the plugin logic.
@@ -62,14 +62,14 @@
             each instance can inherit the methods from the constructor
             function's prototype.
         */
-        this._initialize();
-    }
+		this._initialize();
+	}
 
-    // Avoid Plugin.prototype conflicts
-    $.extend(Plugin.prototype, {
+	// Avoid Plugin.prototype conflicts
+	$.extend(Plugin.prototype, {
 
-        // Initialization logic
-        _initialize: function () {
+		// Initialization logic
+		_initialize: function () {
             /*
                 Create additional methods below and call them via
                 "this.myFunction(arg1, arg2)", ie: "this._buildCache();".
@@ -80,12 +80,12 @@
                 the "Plugin" constructor function (as shown in the _buildCache
                 method below).
             */
-            this._buildCache();
-            this._bindEvents();
-        },
+			this._buildCache();
+			this._bindEvents();
+		},
 
-        // Remove plugin instance completely
-        _destroy: function () {
+		// Remove plugin instance completely
+		_destroy: function () {
             /*
                 The _destroy method unbinds all events for the specific instance
                 of the plugin, then removes all plugin data that was stored in
@@ -98,24 +98,24 @@
                 Consequently, the _destroy method can be called using:
                 $('selector').data('plugin_myPluginName')._destroy();
             */
-            this._unbindEvents();
-            this.$_element.removeData();
-        },
+			this._unbindEvents();
+			this.$_element.removeData();
+		},
 
-        // Cache DOM nodes for performance
-        _buildCache: function () {
+		// Cache DOM nodes for performance
+		_buildCache: function () {
             /*
                 Create variable(s) that can be accessed by other plugin
                 functions. For example, "this.$_element = $(this._element);"
                 will cache a jQuery reference to the element that initialized
-                the plugin. Cached variables can then be used in other methods. 
+                the plugin. Cached variables can then be used in other methods.
             */
-            this.$_element = $(this._element);
-        },
+			this.$_element = $(this._element);
+		},
 
-        // Bind events that trigger methods
-        _bindEvents: function () {
-            var plugin = this;
+		// Bind events that trigger methods
+		_bindEvents: function () {
+			var plugin = this;
             /*
                 Bind event(s) handlers that trigger other functions, ie:
                 "plugin.$_element.on('click', function() {});". Note the use of
@@ -125,25 +125,25 @@
                 This allows us to unbind plugin-specific events using the
                 _unbindEvents method below.
             */
-            plugin.$_element.on('click' + '.' + plugin._pluginName, function () {
+			plugin.$_element.on('click' + '.' + plugin._pluginName, function () {
                 /*
                     Use the "call" method so that inside of the method being
                     called, ie: "_someOtherFunction", the "this" keyword refers
                     to the plugin instance, not the event handler.
                     More: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call
                 */
-                plugin._someOtherFunction.call(plugin);
-            });
-        },
+				plugin._someOtherFunction.call(plugin);
+			});
+		},
 
-        // Unbind events that trigger methods
-        _unbindEvents: function () {
+		// Unbind events that trigger methods
+		_unbindEvents: function () {
             /*
                 Unbind all events in our plugin's namespace that are attached
                 to "this.$_element".
             */
-            this.$_element.off('.' + this._pluginName);
-        },
+			this.$_element.off('.' + this._pluginName);
+		},
 
         /*
             "_someOtherFunction" is an example of a custom method in your
@@ -154,37 +154,37 @@
             plugin methods this way is less confusing (separation of concerns)
             and makes your code easier to test.
         */
-        // Create custom methods
-        _someOtherFunction: function () {
-            console.log('Function is called.');
-            this._callback();
-        },
+		// Create custom methods
+		_someOtherFunction: function () {
+			console.log('Function is called.');
+			this._callback();
+		},
 
-        _callback: function () {
-            // Cache onComplete option
-            var onComplete = this._settings.onComplete;
+		_callback: function () {
+			// Cache onComplete option
+			var onComplete = this._settings.onComplete;
 
-            if ($.isFunction(onComplete)) {
+			if ($.isFunction(onComplete)) {
                 /*
                     Use the "call" method so that inside of the onComplete
                     callback function the "this" keyword refers to the
                     specific DOM node that called the plugin.
                     More: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call
                 */
-                onComplete.call(this._element);
-            }
-        }
+				onComplete.call(this._element);
+			}
+		}
 
-    });
+	});
 
     /*
         Create a lightweight plugin wrapper around the "Plugin" constructor,
         preventing against multiple instantiations.
         More: http://learn.jquery.com/plugins/basic-plugin-creation/
     */
-    $.fn.myPluginName = function (options) {
-        this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
+	$.fn.myPluginName = function (options) {
+		this.each(function () {
+			if (!$.data(this, "plugin_" + pluginName)) {
                 /*
                     Use "$.data" to save each instance of the plugin in case
                     the user wants to modify it. Using "$.data" in this way
@@ -193,15 +193,15 @@
                     the page. It's a smart way to prevent memory leaks.
                     More: http://api.jquery.com/jquery.data/
                 */
-                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
-            }
-        });
+				$.data(this, "plugin_" + pluginName, new Plugin(this, options));
+			}
+		});
         /*
             "return this;" returns the original jQuery object. This allows
             additional jQuery methods to be chained.
         */
-        return this;
-    };
+		return this;
+	};
 
     /*
         Attach the default plugin options directly to the plugin object. This
@@ -213,9 +213,9 @@
         plugin is initialized, "property" will be set to "myValue".
         More: http://learn.jquery.com/plugins/advanced-plugin-concepts/
     */
-    $.fn.myPluginName.defaults = {
-        property: 'value',
-        onComplete: null
-    };
+	$.fn.myPluginName.defaults = {
+		property  : 'value',
+		onComplete: null
+	};
 
 })(jQuery, window, document);
